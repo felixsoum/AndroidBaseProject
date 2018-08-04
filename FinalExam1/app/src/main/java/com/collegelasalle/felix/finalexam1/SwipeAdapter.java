45eeeec9e57ad43d52f131a962eda5c4b8aec10a
Unknown <felix@felixsoum.com>
2018-08-04 01:53:07 -0400

    Update base project version 2

--- FinalExam1/app/src/main/java/com/collegelasalle/felix/finalexam1/SwipeAdapter.java
+++ FinalExam1/app/src/main/java/com/collegelasalle/felix/finalexam1/SwipeAdapter.java
@@ -17,16 +17,14 @@ public class SwipeAdapter extends FragmentStatePagerAdapter {
             case 0:
                 return new SensorFragment();
             case 1:
-                return new LocationFragment();
-            case 2:
                 return new StorageFragment();
-            case 3:
+            case 2:
                 return new GameFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return 3;
     }
 }
